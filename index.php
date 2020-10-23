ec26540bbde9e4fcea950781263220538a65bf78
livinhigh <ivanjthomas@gmail.com>
2020-10-23 21:43:33 +0530

    changes

--- index.php
+++ index.php
@@ -12,9 +12,24 @@
       <div class="spacer">
       </div>
       <h2>Choose your University</h2>
-      <select class="" name="">
-        <option value="christ">CHRIST (deemed to be University)</option>
-      </select><br>
+<?php
+$conn = new mysqli('127.0.0.1', 'epiz_27033647', '5thsemiwp', 'epiz_27033647_olp');
+
+ $query = "select name from colleges";
+$result = $mysqli -> query($query);
+?>
+
+
+<form>
+  <select>
+     <?php
+       while ($row = $result->fetch_assoc())
+       {
+         echo '<option value=" '.$row['id'].' "> '.$row['name'].' </option>';
+       }
+    ?>
+  </select>
+</form><br>
       <form class="" action="login.html" method="post">
         <input type="submit" class="continue" value="Continue"></input>
 
